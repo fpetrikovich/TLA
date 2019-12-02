85d9488d9cd44cad7ff9920907c52906bda986c6
Gaston Lifschitz <gaslif@gmail.com>
2019-12-01 21:22:11 -0300

    Bash script now saves the c output

--- RunTests.sh
+++ RunTests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./generator < tests/test1.m > output.c && gcc -o test1 output.c && ./test1
-./generator < tests/test2.m > output.c && gcc -o test2 output.c && ./test2
-./generator < tests/test3.m > output.c && gcc -o test3 output.c && ./test3
-./generator < tests/test4.m > output.c && gcc -o test4 output.c && ./test4
-./generator < tests/test5.m > output.c && gcc -o test5 output.c && ./test5
+./generator < tests/test1.m > outputTest1.c && gcc -o test1 outputTest1.c && ./test1
+./generator < tests/test2.m > outputTest2.c && gcc -o test2 outputTest2.c && ./test2
+./generator < tests/test3.m > outputTest3.c && gcc -o test3 outputTest3.c && ./test3
+./generator < tests/test4.m > outputTest4.c && gcc -o test4 outputTest4.c && ./test4
+./generator < tests/test5.m > outputTest5.c && gcc -o test5 outputTest5.c && ./test5
 
