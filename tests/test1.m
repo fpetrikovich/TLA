e1d693be78d8fec9afec7f06ae41ae5e1155720f
Gaston Lifschitz <gaslif@gmail.com>
2019-12-01 21:04:20 -0300

    Tests added

---
+++ tests/test1.m
@@ -0,0 +1,12 @@
+start
+number var;
+number n;
+summation(var; n = 1; var + n; n = 10);
+string successMsg = "Summation test success!\n";
+string errorMsg   = "Summation test error..\n";
+if(var == 2036){
+    print(successMsg);
+}else{
+    print(errorMsg);
+}
+end
---
+++ tests/test3.m
@@ -0,0 +1,19 @@
+start 
+number var  = 10;
+number var2 = 12; 
+string msg="Comparation testing passed!\n";
+string error="Error!\n";
+if (var2 >= var){
+	print(msg);
+}
+else{
+	print(error);
+}
+
+if( var2 <= var + 3 ){
+	print(msg);
+}
+else{
+	print(error);
+}
+end 
