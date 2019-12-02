100d8d6a0df1b1cf89558439e38dc258f1f05245
Cristobal Rojas <crojas@itba.edu.ar>
2019-12-01 22:46:46 -0300

    added two test

--- RunTests.sh
+++ RunTests.sh
@@ -5,4 +5,6 @@
 ./generator < tests/test3.m > outputTest3.c && gcc -o test3 outputTest3.c && ./test3
 ./generator < tests/test4.m > outputTest4.c && gcc -o test4 outputTest4.c && ./test4
 ./generator < tests/test5.m > outputTest5.c && gcc -o test5 outputTest5.c && ./test5
+./generator < tests/test6.m > outputTest6.c && gcc -o test6 outputTest6.c && ./test6
+./generator < tests/test7.m > outputTest7.c && gcc -o test7 outputTest7.c && ./test7
 
